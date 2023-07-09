efa4056920437aaccbb922206bb5be037e84200f
Miles Davis <foldes@mail.com>
2023-07-10 00:24:27 +0200

    footer and further styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import './global.css';
+import './Global.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header> 
-        <p className='header-main-text'> caseLink </p>
+      <header className='d-flex align-items-start row'>
+        <div className='header-main-text m-5 col-4 align-items-start row'>
+          <p className='col-6'> caselink. </p>
+        </div>
       </header>
-      <footer></footer>
+      <Container className='d-flex align-items-start'>
+        <div className='link-to-buttons'>
+          <div className='link-to-one d-flex align-items-start'>
+            <p> Browse Cases </p>
+          </div>
+          <div className='link-to-one d-flex align-items-start '>
+            <p> Notifications </p>
+          </div>
+          <div className='link-to-one d-flex align-items-start '>
+            <p> My cases </p>
+          </div>
+          <div className='link-to-one d-flex align-items-start '>
+            <p> Profile </p>
+          </div>
+        </div>
+      </Container>
+      <footer>
+        <div className='info-text p-5'>
+          <p> @ 2023 </p>
+        </div>
+      </footer>
     </div>
   );
 }
