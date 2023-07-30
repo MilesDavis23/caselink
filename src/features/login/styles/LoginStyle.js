bbe69ef88dbf6e61c42cf3df86bdbabf142de6c6
Miles Davis <foldes@mail.com>
2023-07-31 00:50:27 +0200

    lawyer side set up

--- src/features/login/styles/LoginStyle.js
+++ src/features/login/styles/LoginStyle.js
@@ -13,25 +13,9 @@ const loginPaperTextStyle = {
     color: '#FFFDF7'
 }
 
-/* Scroll bar style:  */
-const scrollBarStyle = {
-    '&::-webkit-scrollbar': {
-        width: '0px',
-    },
-    '&::-webkit-scrollbar-track': {
-        background: '#f1f1f1',
-    },
-    '&::-webkit-scrollbar-thumb': {
-        background: '#888',
-    },
-    '&::-webkit-scrollbar-thumb:hover': {
-        background: '#555',
-    },
-}
 
 export {
     loginPaperStyle,
     loginPaperTextStyle, 
-    scrollBarStyle
 }
 
