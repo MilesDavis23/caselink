aff47ee0cc746048079b49a8f0864c7f27a29730
Miles Davis <foldes@mail.com>
2023-08-01 00:52:43 +0200

    simple login implemented

--- src/features/login/components/LoginPanel.js
+++ src/features/login/components/LoginPanel.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import {  useTheme } from '@mui/material/styles';
 import { Grid, Paper, TextField, Button, ButtonBase } from "@mui/material";
 import { loginPaperStyle } from "../styles/LoginStyle";
+import { AuthContext } from "../authentication/AuthContext";
 
 function LoginPanel(){
     const theme = useTheme();
+    const { login } = useContext(AuthContext);
+    const [ email, setEmail ] = useState('');
+
+    const handleTextInputChange = (event) => {
+        setEmail(event.target.value)
+        console.log(email)
+    };
+
+    function handleLogin(){
+        if (email === 'admin'){
+            login()
+        };
+    }
 
     return (
         <>
@@ -12,10 +26,10 @@ function LoginPanel(){
                 <Paper elevation={10} sx={{ background: theme.palette.background.paper,  }} style={loginPaperStyle}>
                     <Grid container direction="column" justifyContent="center" alignItems='center' spacing={2} >
                         <Grid item sx={{width:'100%'}}>
-                            <TextField id="" sx={{width:'100%'}} label="Email address" variant="outlined"></TextField>
+                            <TextField id="" sx={{width:'100%'}} label="Email address" variant="outlined" value={email} onChange={handleTextInputChange}></TextField>
                         </Grid>
                         <Grid item xs={12} sx={{width:'100%'}}>
-                            <Button variant='outlined' sx={{width:'100%'}}> LOGIN </Button>
+                            <Button variant='outlined' sx={{width:'100%'}} onClick={handleLogin} > LOGIN </Button>
                         </Grid>
                         <Grid item xs={12} sx={{width:'100%'}}>
                             <Button variant='outlined' sx={{width:'100%'}} > REGISTER </Button>
