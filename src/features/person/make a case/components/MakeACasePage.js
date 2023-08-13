de7a6e2376043c56c6ffef95fb18979c94c73930
Miles Davis <foldes@mail.com>
2023-08-13 23:02:50 +0200

    make a case

--- src/features/person/make a case/components/MakeACasePage.js
+++ src/features/person/make a case/components/MakeACasePage.js
@@ -4,20 +4,18 @@ import {
     Container,
     Typography,
     Box,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
     Button,
     TextField,
     FormControl,
-    Input,
-    InputLabel,
-    InputAdornment,
+    Select,
+    OutlinedInput, 
+    Chip,
+    MenuItem,
+    InputLabel
 } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
-import LibraryBooksSharpIcon from '@mui/icons-material/LibraryBooksSharp';
+import { MenuProps, getStyles } from '../../../lawyer/individual case page/style/ChipStyle';
 
 
 
@@ -47,131 +45,73 @@ function MakeACase() {
         <>
             <Container justifyContent='center' alignItems="" sx={{ padding: 2, width: '70%' }}>
                 <Grid container justifyContent="space-between" alignItems="center" sx={{ borderBottom: '1px solid white', paddingBottom: 2 }}>
-                    <Grid item xs={6}>
+                    <Grid item xs={12}>
                     <TextField
-                                    sx={{ width: '100%', margin: 1 }}
+                                    sx={{ width: '100%', margin: 0 }}
                                     id="outlined-multiline-static"
                                     label=""
                                     multiline
                                     rows={1}
-                                    defaultValue="Enter tytle here..."
+                                    placeholder="Enter title..."
                                 />
                     </Grid>
                 </Grid>
 
                 <Grid container sx={{ paddingY: 2, borderBottom: '1px solid white' }}>
-                    <Grid item>
-                        <Typography variant="body2">
-                            Vivamus eu malesuada neque, vitae tempor nulla.
-                            Quisque efficitur elit vitae nulla pulvinar, sed faucibus tellus semper.
-                            Nullam sed ultricies leo. Morbi elementum tellus et enim egestas vulputate.
-                            Pellentesque rutrum orci sit amet ante eleifend mollis. Ut aliquet nisl felis, eget tempor ipsum hendrerit sit amet.
-                            Praesent aliquet elit ut egestas hendrerit. In vitae nunc aliquet, finibus purus a, pellentesque lorem.
-                            In magna libero, interdum eget scelerisque non, porta ac nibh. Vestibulum iaculis mi ac libero convallis pretium.
-                            Nunc vehicula lectus et sapien dictum, ut venenatis quam gravida.
-                            Vivamus elementum egestas eleifend. Sed semper magna vitae tortor aliquam tincidunt.
-                            Nulla ut urna tempus, rhoncus orci ac, tempus risus. Donec condimentum lacus dui, eget congue felis elementum bibendum.
-                            Nunc blandit tortor congue odio convallis mollis.
-                            <Box sx={{ padding: 1 }}></Box>
-                            {'So this is part is very importan.'}
-                            <Box sx={{ padding: 2 }}></Box>
-                            Vivamus eu malesuada neque, vitae tempor nulla.
-                            Quisque efficitur elit vitae nulla pulvinar, sed faucibus tellus semper.
-                            Nullam sed ultricies leo. Morbi elementum tellus et enim egestas vulputate.
-                            Pellentesque rutrum orci sit amet ante eleifend mollis. Ut aliquet nisl felis, eget tempor ipsum hendrerit sit amet.
-                            Praesent aliquet elit ut egestas hendrerit. In vitae nunc aliquet, finibus purus a, pellentesque lorem.
-                            In magna libero, interdum eget scelerisque non, porta ac nibh. Vestibulum iaculis mi ac libero convallis pretium.
-                            Nunc vehicula lectus et sapien dictum, ut venenatis quam gravida.
-                            Vivamus elementum egestas eleifend. Sed semper magna vitae tortor aliquam tincidunt.
-                            Nulla ut urna tempus, rhoncus orci ac, tempus risus. Donec condimentum lacus dui, eget congue felis elementum bibendum.
-                            Nunc blandit tortor congue odio convallis mollis.
-                            <Box sx={{ padding: 0.5 }}></Box>
-                            Vivamus eu malesuada neque, vitae tempor nulla.
-                            Quisque efficitur elit vitae nulla pulvinar, sed faucibus tellus semper.
-                            Nullam sed ultricies leo. Morbi elementum tellus et enim egestas vulputate.
-                            Pellentesque rutrum orci sit amet ante eleifend mollis. Ut aliquet nisl felis, eget tempor ipsum hendrerit sit amet.
-                            Praesent aliquet elit ut egestas hendrerit. In vitae nunc aliquet, finibus purus a, pellentesque lorem.
-                            In magna libero, interdum eget scelerisque non, porta ac nibh. Vestibulum iaculis mi ac libero convallis pretium.
-                            Nunc vehicula lectus et sapien dictum, ut venenatis quam gravida.
-                            Vivamus elementum egestas eleifend. Sed semper magna vitae tortor aliquam tincidunt.
-                            Nulla ut urna tempus, rhoncus orci ac, tempus risus. Donec condimentum lacus dui, eget congue felis elementum bibendum.
-                            Nunc blandit tortor congue odio convallis mollis.
-                        </Typography>
-                    </Grid>
-                </Grid>
-
-                <Grid container sx={{ marginTop: '20px', marginBottom: '20px', border: '1px solid white', borderRadius: '5px' }}>
                     <Grid item xs={12}>
-                        <Box sx={{ width: 1, bgcolor: 'background.paper' }}>
-                            <Box sx={{ width: 1, borderBottom: '1px solid white', padding: '10px' }} >
-                                <Typography variant="h6" component="div">
-                                    Uploaded Documents
-                                </Typography>
-                            </Box>
-                            <List sx={{ marginLeft: 5 }}>
-                                <ListItem disablePadding>
-                                    <ListItemIcon>
-                                        <LibraryBooksSharpIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary="Rental Contract" />
-                                </ListItem>
-                                <ListItem disablePadding>
-                                    <ListItemIcon>
-                                        <LibraryBooksSharpIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary="Authorization" />
-                                </ListItem>
-                                <ListItem disablePadding>
-                                    <ListItemIcon>
-                                        <LibraryBooksSharpIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary="Title Deed" />
-                                </ListItem>
-                            </List>
-                        </Box>
-                    </Grid>
-                </Grid>
-
-                <Grid container sx={{ marginTop: '20px', marginBottom: '20px', border: '1px solid white', borderRadius: '5px' }}>
-                    <Grid item xs={12}>
-                        <Box sx={{ width: 1, bgcolor: 'background.paper' }}>
-                            <Box sx={{ width: 1, borderBottom: '1px solid white', padding: '10px' }} >
-                                <Typography variant="h6" component="div">
-                                    Send Offer
-                                </Typography>
-                            </Box>
-                            <List sx={{ margin: 2 }}  >
-                                <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                                    <InputLabel htmlFor="standard-adornment-amount">Offer Amount</InputLabel>
-                                    <Input
-                                        id="standard-adornment-amount"
-                                        startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                                    />
-                                </FormControl>
-                                <TextField
-                                    sx={{ width: '100%', margin: 1 }}
+                    <TextField
+                                    sx={{ width: '100%', margin: 0 }}
                                     id="outlined-multiline-static"
                                     label=""
                                     multiline
-                                    rows={4}
-                                    defaultValue="Your offer goes here..."
+                                    placeholder="Explain your problem..."
                                 />
+                    </Grid>
+                </Grid>
 
-
-
-
-                                <Button variant='contained' sx={{ width: 1, margin: 1 }} > Send Offer </Button>
-                            </List>
+                <Grid container sx={{ marginTop: '20px', marginBottom: '20px', border: '1px solid white', borderRadius: '5px' }}>
+                    <Grid item xs={12}>
+                        <Box sx={{ width: 1, bgcolor: 'background.paper' }}>
+                            <Box sx={{ width: 1, borderBottom: '1px solid white', padding: '10px' }} >
+                                <Typography variant="h6" component="div">
+                                    Upload Documents
+                                </Typography>
+                            </Box>
+                            <FormControl sx={{ m: 0, width: '100%' }}>
+                                    <InputLabel htmlFor="select-multiple-chip" style={{ visibility: 'hidden' }}>Hidden Label</InputLabel>
+                                    <Select
+                                        id=''
+                                        multiple
+                                        value={personName}
+                                        onChange={handleChange}
+                                        input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                                        renderValue={(selected) => (
+                                            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                                {selected.map((value) => (
+                                                    <Chip key={value} label={value} />
+                                                ))}
+                                            </Box>
+                                        )}
+                                        MenuProps={MenuProps}
+                                    >
+                                        {names.map((name) => (
+                                            <MenuItem
+                                                key={name}
+                                                value={name}
+                                                style={getStyles(name, personName, theme)}
+                                            >
+                                                {name}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
                         </Box>
                     </Grid>
                 </Grid>
 
                 <Grid container alignItems="center" justifyContent="space-between" sx={{ width: '100%' }} >
-                    <Grid item xs={5} sx={{ marginRight: 2 }}>
-                        <Button variant='contained' sx={{ width: 1 }} > Back </Button>
-                    </Grid>
-                    <Grid item xs={5} sx={{ marginRight: 0 }}>
-                        <Button component={Link} to="/lawyer/my-cases/" variant='contained' sx={{ width: 1 }}> Add To My Cases </Button>
+                    <Grid item xs={12} sx={{ marginRight: 0 }}>
+                        <Button component={Link} to="" variant='contained' sx={{ width: 1 }}> Submit Case </Button>
                     </Grid>
                 </Grid>
             </Container>
