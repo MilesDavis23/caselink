8aae498bfdf3954823a546dcc874684600e1085f
Miles Davis <foldes@mail.com>
2023-08-13 20:01:02 +0200

    send offer panel implemented

---
+++ src/features/lawyer/individual case page/style/ChipStyle.js
@@ -0,0 +1,27 @@
+
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        sryle: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250, 
+        }
+    }
+};
+
+
+function getStyles(name, personName, theme) {
+    return {
+        fontWeigth:
+            personName.indexOf(name) === -1
+            ? theme.typography.fontWeigthRegular
+            : theme.typography.fontWeigthMedium,
+    };
+}
+
+export {
+    getStyles, 
+    MenuProps
+}
