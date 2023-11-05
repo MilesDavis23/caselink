2460d0085502875ad343bb37419b7626af184f37
Miles Davis <foldes@mail.com>
2023-11-05 21:05:31 +0100

    tags

--- src/features/person/make a case/functions/axios.js
+++ src/features/person/make a case/functions/axios.js
@@ -2,15 +2,16 @@ import axios from "axios";
 
 const endpoint = 'http://localhost:3002/make-a-case/create';
 
-function createACase( title, briefDescription, detailedDescription, caseCategory, fileUrl, fileType) {
+function createACase( title, briefDescription, detailedDescription, caseCategory, categoryTags, timestamp) {
     return axios.post(endpoint, 
         {
             title, 
             briefDescription, 
             detailedDescription, 
-            caseCategory, 
-            fileUrl, 
-            fileType
+            caseCategory,
+            categoryTags,
+            timestamp
+            
         },
         {
             headers: {
